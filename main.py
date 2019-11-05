a8a87cdb8e6019864a4b581cee04a1be71920e0b
fandoghi <55033503+fandoghi@users.noreply.github.com>
2019-11-05 11:26:16 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+double = lambda x : x *2
+x=double(5)
+print(x)
+
+double = lambda x : x +2
+x=double(5)
+print(x)
+
+double = lambda x : x -2
+x=double(5)
+print(x)
+
+double = lambda x : x /2
+x=double(5)
+print(x)
+
+double = lambda x : x *2
+x=double(5)
+print(x)
+
+double = lambda x : x %2
+x=double(5)
+print(x)
+
+double = lambda x : x //2
+x=double(5)
+print(x)
+
+double = lambda x : x **2
+x=double(5)
+print(x)
